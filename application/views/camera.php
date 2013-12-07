228726464e2a0604b8cf2c24cc3e06038e488b7a
Ben Christensen <bmchrist@mtu.edu>
2013-12-07 05:37:24 -0500

    camera blah

--- application/views/camera.php
+++ application/views/camera.php
@@ -1,4 +1,4 @@
 <?php $this->load->view('layout/header'); ?>
 <img src="/?action=stream">
-<?php echo base_url() ?>
+<?php echo $_SERVER['HTTP_HOST'] ?>:8080
 <?php $this->load->view('layout/footer'); ?>
